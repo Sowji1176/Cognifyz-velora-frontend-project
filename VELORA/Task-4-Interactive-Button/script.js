aeb5984881efbdaf7393b2fcfc48aec793cb20ce
Sowjanya <kuppalalakshmisowjanya123@gmail.com>
2026-06-09 11:53:35 +0530

    Add files via upload

---
+++ VELORA/Task-4-Interactive-Button/script.js
@@ -0,0 +1,19 @@
+```javascript
+function changeTheme(){
+
+    const body = document.getElementById("body");
+
+    if(body.style.backgroundColor === "rgb(20, 20, 20)"){
+
+        body.style.backgroundColor = "#000000";
+
+    }
+
+    else{
+
+        body.style.backgroundColor = "#141414";
+
+    }
+
+}
+```
---
+++ VELORA/Task-5-API-Integration/script.js
@@ -0,0 +1,21 @@
+```javascript
+// Change Theme Function
+
+function changeTheme() {
+
+    var body = document.getElementById("body");
+
+    if (body.style.backgroundColor == "rgb(20, 20, 20)") {
+
+        body.style.backgroundColor = "#000000";
+
+    } 
+    
+    else {
+
+        body.style.backgroundColor = "#141414";
+
+    }
+
+}
+```
